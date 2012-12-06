2e48601429c34dfc689d03a6a5031d884a1741b0
David Strauss <david@davidstrauss.net>
2012-12-06 21:34:48 +0000

    Add log files.

---
+++ src/log.c
@@ -0,0 +1,41 @@
+/***
+  This file is part of fusedav.
+
+  fusedav is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  fusedav is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with fusedav; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+***/
+
+#include <systemd/sd-journal.h>
+
+#include "log.h"
+
+static int minimum_verbosity;
+
+void log_set_minimum_verbosity(int verbosity) {
+    minimum_verbosity = verbosity;
+}
+
+int log_print(int verbosity, const char *format, ...) {
+        int r;
+        va_list ap;
+
+        if (verbosity < minimum_verbosity)
+            return 0;
+
+        va_start(ap, format);
+        r = sd_journal_printv(verbosity, format, ap);
+        va_end(ap);
+
+        return r;
+}
