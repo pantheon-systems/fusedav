ec6255ad9cae6b1be2fec6db99102ece31262553
David Strauss <david@davidstrauss.net>
2013-02-21 10:44:10 +0000

    Drop LevelDB LRU to 5MB.

--- src/statcache.c
+++ src/statcache.c
@@ -151,7 +151,7 @@ int stat_cache_open(stat_cache_t **cache, struct stat_cache_supplemental *supple
     supplemental->options = leveldb_options_create();
 
     // Initialize LevelDB's LRU cache.
-    supplemental->lru = leveldb_cache_create_lru(100 * 1048576); // 100MB
+    supplemental->lru = leveldb_cache_create_lru(5 * 1048576); // 5MB
     leveldb_options_set_cache(supplemental->options, supplemental->lru);
 
     // Create the database if missing.
