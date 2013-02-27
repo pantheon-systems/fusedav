254a7b3ed92bde99ec75974e58d10b592d852343
Jerry Blakley <jerry@getpantheon.com>
2013-02-27 21:30:28 +0000

    minor

--- src/ldb-filecache.c
+++ src/ldb-filecache.c
@@ -652,7 +652,7 @@ int ldb_filecache_release(ldb_filecache_t *cache, const char *path, struct fuse_
 
     log_print(LOG_DEBUG, "Done syncing file (%s) for release, calling ldb_filecache_close.", path);
 
-     ret = 0;
+    ret = 0;
 
 finish:
 
