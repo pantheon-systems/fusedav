3080e330b4447b2481ea35e4fc644c2c15c370d0
Jerry Blakley <jerry@getpantheon.com>
2013-06-19 22:36:53 +0000

    Put DavServer in test_dav.py to avoid issues with where to find it

--- tests/test_dav.py
+++ tests/test_dav.py
@@ -25,10 +25,9 @@ import sys
 
 from sh import cp, mv
 
-try:
-    from tests.dav_server import DavServer, PYWEBDAV_HOST, PYWEBDAV_PORT
-except:
-    from titan.pantheon.tests.lib.dav_server import DavServer, PYWEBDAV_HOST, PYWEBDAV_PORT
+from multiprocessing import Process
+from pywebdav.server import server
+from pywebdav.server.fileauth import DAVAuthHandler
 
 # You can execute this test by executing:
 #   cd to your /opt/<fusedav> directory and execute 'trial ./tests/test_dav.py'
@@ -76,6 +75,47 @@ DAV_CLIENT = 'fusedav'
 # REVIEW: would this affect basic functionality?
 # NB: this config is really for the upcoming new version of fusedav with a config file
 FUSEDAV_CONFIG = 'nodaemon,noexec,atomic_o_trunc,hard_remove,umask=0007,cache_path='
+
+PYWEBDAV_HOST = '127.0.0.1'
+PYWEBDAV_PORT = '8008'
+
+class DavServer:
+    def __init__(self, path):
+        self.path = path
+
+    def start(self):
+        self.t = Process(target=self.run)
+        self.t.start()
+
+    def stop(self):
+        self.t.terminate()
+        self.t.join()
+
+    def run(self):
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+        conf = server.setupDummyConfig(**{
+            'verbose': False,
+            'directory': self.path,
+            'port': PYWEBDAV_PORT,
+            'host': PYWEBDAV_HOST,
+            'noauth': True,
+            'user': '',
+            'password': '',
+            'daemonize': False,
+            'daemonaction': 'start',
+            'counter': 0,
+            'lockemulation': False,
+            'mimecheck': True,
+            'chunked_http_response': True,
+            'http_request_use_iterator': 0,
+            'http_response_use_iterator': True,
+            'baseurl': 0
+        })
+        handler = DAVAuthHandler
+        handler._config = conf
+        logging.getLogger().setLevel(logging.ERROR)
+        server.runserver(directory=self.path, noauth=True, handler=handler)
 
 class TestDav(unittest.TestCase):
     files_root = None
