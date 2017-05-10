4eb3dcd6a0909793abc25a5ad0df08c20751d330
Jerry Blakley <jerry@getpantheon.com>
2017-05-10 20:55:58 +0000

    New and improved server tests

--- tests/server-tests/api_test.go
+++ tests/server-tests/api_test.go
@@ -107,6 +107,20 @@ func testMethod(t *testing.T, testInput TestInput) error {
 		}
 	}
 
+	if testInput.method == "GET" { // Are there other methods which get content. We could genericize
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			// handle error
+			t.Errorf("testMethod: Error on ioutil.ReadAll: %v", err)
+		}
+
+		if testInput.expectedStatusCode != 404 &&
+			len(testInput.content) > 0 &&
+			string(body) != testInput.content {
+			t.Errorf("testMethod: Error, expected content %v, got %v", testInput.content, resp.Body)
+		}
+	}
+
 	testOutput.status = resp.Status
 	testOutput.statusCode = resp.StatusCode
 
