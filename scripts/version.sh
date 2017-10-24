489700aed426d5009c776e1679dbf15addc9b225
Jerry Blakley <jerry@pantheon.io>
2017-10-24 17:14:15 +0000

    Stage to master (#401)
    * Revert earlier partial file transfer fix; increase filesystem string size (#395) (#396) (#397)
    
    * Ensure version.sh finds the Linux entry (#400)
    
    * Stage fix version sh (#402)
    
    * Ensure version.sh finds the Linux entry
    
    * Add ruby version to circle.yml

--- scripts/version.sh
+++ scripts/version.sh
@@ -8,7 +8,7 @@ if [ ! -d "$HOME/bin" ]; then
 fi
 
 if [ ! -f "$HOME/bin/autotag" ]; then
-  AUTOTAG_URL=$(curl -silent -o - -L https://api.github.com/repos/pantheon-systems/autotag/releases/latest | grep 'browser_' | cut -d\" -f4 | awk '{print $0}')
+  AUTOTAG_URL=$(curl -silent -o - -L https://api.github.com/repos/pantheon-systems/autotag/releases/latest | grep 'browser_' | grep 'Linux' | cut -d\" -f4 | awk '{print $0}')
   # handle the off chance that this wont work with some pre-set version
   if [ -z "$AUTOTAG_URL" ] ;  then
     AUTOTAG_URL="https://github.com/pantheon-systems/autotag/releases/download/v0.0.3/autotag.linux.x86_64"
