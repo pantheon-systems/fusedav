eebded7c36dadaac28729dd9557f5d6097dc5925
Will Milton <wa.milton@gmail.com>
2019-05-02 15:26:45 +0000

    fixup

--- src/session.c
+++ src/session.c
@@ -264,6 +264,8 @@ static void print_errors(const int iter, const char *type_str, const char *fcn_n
 
     asprintf(&metric_str, "%s.%s", fcn_name, curl_status_str);
     stats_timer(metric_str, elapsed_time);
+    free(metric_str);
+    free(curl_status_str);
     // Don't treat slow requests as 'failures'; it messes up the failure/recovery stats
     if (!slow_request) {
         char *failure_str = NULL;
