13dc21d9a6e342d7c253dd1a6d8618e793704d5e
Will Milton <wa.milton@gmail.com>
2019-08-06 13:33:36 +0000

    add iozone and sources and tests to devel builds

--- scripts/docker-inner.sh
+++ scripts/docker-inner.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 #
 set -ex
@@ -13,6 +13,11 @@ fusedav_channel=$1
 rpm_dir=$2
 build=$3
 epoch=$4
+
+devel=
+if [[ "$fusedav_channel" == *-devel ]]; then
+    devel=true
+fi
 
 fedora_release=$(rpm -q --queryformat '%{VERSION}\n' fedora-release)
 GITSHA=$(git log -1 --format="%h")
@@ -64,9 +69,30 @@ if [ "1" != "$?" ] ; then
 fi
 set -e
 
+# pack in sources and tests into the rpm
+if [[ -n $devel ]]; then
+    cp -r -t $install_prefix src tests
+
+    iozone_version=iozone3_487
+    curl http://www.iozone.org/src/current/${iozone_version}.tar > ${iozone_version}.tar
+    sha256sum -c sha256sum
+
+    tar xf ${iozone_version}.tar
+
+    pushd 2>&1 ${iozone_version}/src/current > /dev/null
+    make linux-AMD64
+    mv iozone $install_prefix/iozone
+    popd 2>&1 > /dev/null
+fi
+
 mv /usr/local/bin/fusedav $install_prefix/$name
 cp $bin/exec_wrapper/mount.fusedav_chan /usr/sbin/mount.$name
 chmod 755 /usr/sbin/mount.$name
+
+DEP_GCC=
+if [[ -n $devel ]]; then
+    DEP_GCC="--depends gcc"
+fi
 
 fpm -s dir -t rpm \
   --name "${name}" \
@@ -76,10 +102,11 @@ fpm -s dir -t rpm \
   --url "${url}" \
   --vendor "${vendor}" \
   --description "${description}" \
-  --depends  uriparser \
+  --depends uriparser \
   --depends fuse-libs \
   --depends leveldb \
   --depends jemalloc \
+  ${DEP_GCC} \
   --log=debug \
   $install_prefix \
   /usr/sbin/mount.$name
